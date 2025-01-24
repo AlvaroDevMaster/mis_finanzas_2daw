74191dc783aac79d110682d7ce5cca797654627d
AlvaroDevMaster <alvaromaqueda6@outlook.com>
2025-01-24 11:30:23 +0000

    Tarea 2

---
+++ resources/views/components/button.blade.php
@@ -0,0 +1,14 @@
+@if ($attributes->has('href'))
+    <a href="{{ $href }}"
+        class="px-4 py-2 font-semibold text-white bg-blue-500 rounded-lg shadow hover:bg-blue-600 focus:outline-none">
+        {{ $slot }}
+    </a>
+@elseif ($attributes->has('name'))
+<input type="submit"
+        {{ $attributes }}
+        class="px-4 py-2 font-semibold text-white bg-blue-500 rounded-lg shadow hover:bg-blue-600 focus:outline-none">
+        {{ $slot }}
+</input>
+@else
+   <button type="button" class="px-4 py-2 font-semibold text-white bg-blue-500 rounded-lg shadow hover:bg-blue-600 focus:outline-none">{{$slot}}</button> 
+@endif
--- resources/views/income/index.blade.php
+++ resources/views/income/index.blade.php
@@ -1,4 +1,5 @@
 
 <x-layouts.index :title="$title">
   <x-table :tableData="$tableData" />
+  <x-button name="add_income"></x-button>
 </x-layouts.index>
